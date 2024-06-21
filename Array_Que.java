ebb30a23528362e91445337c26e23a6b13d93904
manseesahu <Mansee7788@gmail.com>
2024-06-21 22:56:23 +0530

    Mnn

---
+++ Array_Que.java
@@ -0,0 +1,17 @@
+package Array_Question;
+
+public class Print_N {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]=new int[3];//declaration and instantiation  
+		a[0]=7;//initialization  
+		a[1]=4;  
+		a[2]=1;  
+
+		for(int i=0;i<a.length;i++)//length is the property of array  
+		System.out.print(a[i]);  
+		}
+	}
+
